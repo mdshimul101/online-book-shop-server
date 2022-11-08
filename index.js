26cda69c65d22aa0db83cbdacdc0afb2e5df4451
mdshimul101 <shimulfjr101@gmail.com>
2022-11-08 23:09:01 +0600

    create api for all book

--- index.js
+++ index.js
@@ -26,17 +26,23 @@ async function run() {
 
     app.get("/books", async (req, res) => {
       const query = {};
+      const cursor = serviceCollection.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/allBooks", async (req, res) => {
+      const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
     });
 
-    // app.get("/services/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const service = await serviceCollection.findOne(query);
-    //   res.send(service);
-    // });
+    app.get("/allBooks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
 
     // orders api
     // app.get("/orders", async (req, res) => {
