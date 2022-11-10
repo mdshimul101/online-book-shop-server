1e02e130baff75856d2c9d227239b3fc23d3ed72
mdshimul101 <shimulfjr101@gmail.com>
2022-11-10 20:16:21 +0600

    JWT Token add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -17,6 +18,23 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     const serviceCollection = client
@@ -24,6 +42,15 @@ async function run() {
       .collection("booksCollection");
     const reviewCollection = client.db("bookShop").collection("allReviews");
     const newbooksCollection = client.db("bookShop").collection("newBooks");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ token });
+    });
 
     app.get("/books", async (req, res) => {
       const query = {};
@@ -60,7 +87,11 @@ async function run() {
       res.send(service);
     });
 
-    app.get("/allReviews", async (req, res) => {
+    app.get("/allReviews", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ message: "unauthorized access" });
+      }
       let query = {};
 
       if (req.query.email) {
